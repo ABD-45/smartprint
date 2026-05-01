bf9b8fbb87512b3f60fbffb188fae5b7e71bcf6e
abdul <231501003@rajalakshmi.edu.in>
2026-05-01 12:26:46 +0530

    method updated

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const corsOptions = {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   credentials: true,
 };
 
